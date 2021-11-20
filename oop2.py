c44b25cdd458c8271a1c432f7beb1d47eb16c586
Abbosbek Ustaboyev Botirjonovich <91521657+Abbosbekdev@users.noreply.github.com>
2021-11-20 09:43:32 +0500

    Create oop2.py

---
+++ oop2.py
@@ -0,0 +1,58 @@
+from Massiv_class import Massiv
+# 1 - masala
+
+# x = Massiv([2,4,5,6])
+# y = Massiv([2,6,8,4])
+# z = x+y
+# print(z)
+# print(z.max())
+# print(z.min())
+# print(z.sum())
+
+# 2 - masala
+
+# x = Massiv([1,5,6,8,3,9,5])
+# print(x.indeks())
+
+# 3 - masala
+
+# x = Massiv([2,3,4,-8,-2,9,-7,2,-1])
+# print(x.joylashtir())
+
+# 4 - masala
+
+# x = Massiv([2,3,4,-8,-2,9,-7,2,-1])
+# print(x.sana())
+
+# 5 - masala
+
+# x = Massiv([2,4,6,8,5,3,4,8,6])
+# x.propel(2)
+# print(x)
+
+# 6 - masala
+
+# x = Massiv([2,3,4,7,5,8,6,0,9])
+# x.propel1(4)
+# print(x)
+
+# 7 - masala
+
+# x = Massiv([100,304,340,225,315])
+# y = Massiv([5,2,10,15,9])
+# z = x/100*y
+# print(z)
+
+# 8 - masala
+
+# x = Massiv([45,28,56,32,40,65])
+# y = Massiv([1000,6000,7000,2500,3000,10000])
+# z = Massiv([30,10,35,20,36,42])
+# print((x-z)*y)
+# print(x-z)
+
+# 10 - masala
+
+# x = Massiv([])
+# x.teskari(5)
+# pirint(x)
